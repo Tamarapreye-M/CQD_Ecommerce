8db06cbb6e003ff4613eec8e0fd4168345e4e5b1
Tamarapreye-M <preyemichael116@gmail.com>
2024-02-03 23:47:56 +0100

    responsive styles updated

--- js/products.js
+++ js/products.js
@@ -1,68 +1,82 @@
-let url = `https://fakestoreapi.com/products`
+let url = `https://fakestoreapi.com/products`;
 
 function addDom(data) {
-  const container = document.querySelector('#section__two .container');
-  const divs = data.map( item => {
-    return `<div class="item">
-      <img src='${item.image}' />
+	const container = document.querySelector("#section__two .container");
+	const divs = data
+		.map((item) => {
+			console.log(`${item.image.slice(0, -3)}png`);
+
+			return `<div class="item">
+
+      <div class="image-container">
+        <div class="image" >
+          <img src="${item.image}">
+        </div>
+      </div>
+     <div class="text-div">
       <h2>${item.title}</h2>
-      <p class="description">${item.description}</p>
-      <p class="readMoreBtn">Read More</p>
+      <p class="description">${item.description} </p>
+      <span class="readMoreBtn">Read More</span>
+      
       <p class="price">$${item.price}</p>
+      </div>
     </div>
-    ` 
-  }).join(" ");
-  
-  container.innerHTML = divs
+    
+    `;
+		})
+		.join(" ");
 
-  // Read More Function For Item Description
-  const readMoreBtns = document.querySelectorAll('.readMoreBtn');
-  readMoreBtns.forEach((btn, index) => {
-    let isExpanded = false;
-    const description = document.querySelectorAll('.description')[index];
+	container.innerHTML = divs;
 
-    btn.addEventListener('click', function() {
-      if (isExpanded) {
-        description.style.maxHeight = '100px'; // Set back to the collapsed state
-        btn.textContent = 'Read More';
-      } else {
-        description.style.maxHeight = 'none'; // Expand to show all content
-        btn.textContent = 'Read Less';
-      }
+	// Read More Function For Item Description
+	const readMoreBtns = document.querySelectorAll(".readMoreBtn");
+	readMoreBtns.forEach((btn, index) => {
+		let isExpanded = false;
+		const description = document.querySelectorAll(".description")[index];
 
-      isExpanded = !isExpanded;
-    });
-  });
+		btn.addEventListener("click", function () {
+			if (isExpanded) {
+				description.style.maxHeight = "100px"; // Set back to the collapsed state
+				btn.textContent = "Read More";
+			} else {
+				description.style.maxHeight = "none"; // Expand to show all content
+				btn.textContent = "Read Less";
+			}
 
-  console.log(data);
+			isExpanded = !isExpanded;
+		});
+	});
+
+	console.log(data);
 }
 
 const select = document.getElementById("categories");
 
 function handleSelect(e) {
-  if (e.target.value != 'all') {
-    url = `https://fakestoreapi.com/products/category/${e.target.value}`;
-  } 
-  fetchApi(url);
+	if (e.target.value != "all") {
+		url = `https://fakestoreapi.com/products/category/${e.target.value}`;
+	}
+	fetchApi(url);
 
-  console.log(e.target.value);
-  console.log(url);
+	console.log(e.target.value);
+	console.log(url);
 }
 
-select.addEventListener('click', handleSelect);
+select.addEventListener("click", handleSelect);
 
 async function fetchApi(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    addDom(data);
-
-  } catch (error) {
-    console.error('Error fetching or parsing data:', error);
-  };
-};
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error("Network response was not ok");
+		}
+		const data = await response.json();
+		addDom(data);
+	} catch (error) {
+		console.error("Error fetching or parsing data:", error);
+	}
+}
 
 fetchApi(url);
+
+// style="background-image: url(${item.image}); height: 150px; width: 100%; background-size: contain; background-repeat: no-repeat; background-position: center;"
